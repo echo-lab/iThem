d6ed23f8ed849c6c596cc47478147291d757f819
Marx <boyuan@vt.edu>
2022-03-21 18:58:31 -0400

    ifttt backend mvp

--- pages/api/ifttt/v1/actions/trigger_a_inlet/index.js
+++ pages/api/ifttt/v1/actions/trigger_a_inlet/index.js
@@ -1,24 +1,104 @@
-// const { connectToDatabase } = require("../../../lib/mongodb");
-// const ObjectId = require("mongodb").ObjectId;
+const { connectToDatabase } = require("../../../../../../lib/mongodb");
+const ObjectId = require("mongodb").ObjectId;
 export default async function handler(req, res) {
   // switch the methods
-//   const { db } = await connectToDatabase();
+  const { db } = await connectToDatabase();
   switch (req.method) {
-    //   http://localhost:3000/api/inlets/update?name=new name&id=61e983a630328ac2f0cca0a4
     case "POST": {
-      try {
-        //   console.log(req.data)
-        // let inlets = await db.collection("inlets").insertOne({
-        //   label:req.query.name,
-        //   email: req.query.email,
-        //   name: req.query.name,
-        //   value: req.query.name,
-        //   description: req.query.description,
-        //   createdAt: new Date(),
-        // });
-        return res.status(200).json({
+      const email = req.body.actionFields.data.email;
 
-        //   message: JSON.parse(JSON.stringify(inlets)),
+      let inlet = await db
+        .collection("inlets")
+        .find({
+          $and: [{ email: email }, { name: req.body.actionFields.inlet }],
+        })
+        .toArray();
+
+      let outlets = await db
+        .collection("outlets")
+        .find({
+          $and: [{ email: email }],
+        })
+        .toArray();
+
+      let variables = await db
+        .collection("variables")
+        .find({
+          $and: [{ email: email }],
+        })
+        .toArray();
+
+      const load = (value) => {
+        const found = variables.find((elm) => elm.name == value);
+        if (typeof found === "undefined")
+          return res.status(400).json({
+            success: false,
+            errors: [
+              { message: "In Function 'load', variable used does not exist" },
+            ],
+          });
+        else {
+          switch (found.type) {
+            case "int":
+              return +found.value;
+            case "double":
+              return +found.value;
+            case "boolean":
+              return found.value == "TRUE";
+            default:
+              return found.value;
+          }
+        }
+      };
+
+      const save = (value, name) => {
+        const found = variables.find((elm) => elm.name == name);
+        if (typeof found === "undefined")
+          return res.status(400).json({
+            success: false,
+            errors: [
+              { message: "In Function 'save', variable used does not exist" },
+            ],
+          });
+        else {
+          fetch(
+            `https://ithem.cs.vt.edu/api/var/update?id=${found._id}&value=${value}`,
+            {
+              method: "POST",
+            }
+          );
+        }
+      };
+
+      const program = (name) => {
+        const found = outlets.find((elm) => elm.name == name);
+        if (typeof found === "undefined")
+          return res.status(400).json({
+            success: false,
+            errors: [
+              {
+                message: "In Function 'Program', variable used does not exist",
+              },
+            ],
+          });
+        else {
+          fetch(
+            `https://ithem.cs.vt.edu/api/events/create?email=${email}&name=${name}`,
+            {
+              method: "POST",
+            }
+          );
+        }
+      };
+
+      if (inlet.length != 1)
+        return res.status(400).json({
+          success: false,
+        });
+      eval(inlet[0].code);
+
+      try {
+        return res.status(200).json({
           success: true,
         });
       } catch (error) {
--- pages/api/ifttt/v1/test/setup.js
+++ pages/api/ifttt/v1/test/setup.js
@@ -1,37 +1,34 @@
-
 export default async function handler(req, res) {
-    // switch the methods    
-    switch (req.method) {
-      case "POST": {
-          if(req.headers['ifttt-service-key']=="iuLesxDNlW33TMnaeS3BWg3ipN2suKMSEj5bhTkl1n3ZdcfuDx3eHdQsG4JPBspb")
-          res.status(200).json({
-            "data": {
-              "accessToken": "ya29.A0ARrdaM-Nq1tB20qDRSQWoUKt5EAFFhsjuV5CekMnR70VpY1QlH-fywCn3BVqbKZl4Gbd49Qd4dUCb8rWToGqkFlqWwqA9vrKhEn9kyY99n3ZoCuYL8794RYIuxyAz00pgxTQczY8lT3I0k4QTL6BFMQaVf0JRr0LACCqxQ",
-              "samples": {
-                "triggers": {
-                  "outlet_handler": {
-                    "name": "outlet4"
+  // switch the methods
+  switch (req.method) {
+    case "POST": {
+      if (
+        req.headers["ifttt-service-key"] ==
+        "iuLesxDNlW33TMnaeS3BWg3ipN2suKMSEj5bhTkl1n3ZdcfuDx3eHdQsG4JPBspb"
+      )
+        res.status(200).json({
+          data: {
+            accessToken:
+              "ya29.A0ARrdaM9GL3HVp22gKKaiJ3T66EKKzdmaagtlPpVS2YEklE1OdWf1wJ4xk_njQCO1k0-1rgLe8ouiy6MbsnrjNfk6B4ZGwX1OUInudmUfGGFHsnoIB0AAEiXr1osgGwky5aO0Y3yu4w50dvJvVc1jjwrEQKSWYS8qrCi7Yg",
+            samples: {
+              triggers: {
+                outlet_handler: {
+                  name: "outlet4",
+                },
+              },
+              actions: {
+                trigger_a_inlet: {
+                  inlet: "testOutlet",
+                  data: {
+                    name: "testOutlet",
+                    email: "boyuan@vt.edu",
                   },
                 },
-                "actions": {
-                  "trigger_a_inlet":{
-                    "inlet":"1",
-                    "data":"test"
-                  }
-                },
-                "actionRecordSkipping": {
-                  "post_a_photo": {
-                    "album": "Sports",
-                    "url": "http://example.com/foo/jpg",
-                    "description": ""
-                  }
-                }
-              }
-            }
-          })
-          else 
-          res.status(401).json({})
-  
-      }
+              },
+            },
+          },
+        });
+      else res.status(401).json({});
     }
   }
+}
